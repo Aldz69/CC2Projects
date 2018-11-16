3896a8e3e2d4f2a6985738b68f208ceebe0d79bf
Aldwin EBrada <44883722+Aldz69@users.noreply.github.com>
2018-11-16 08:15:53 +0800

    Odd/Even Tabke
    Table

---
+++ Arrayy.java
@@ -0,0 +1,33 @@
+package arrayy;
+import java.util.*;
+
+public class Arrayy {
+    
+    
+    public static void main(String[] args) {
+   int [] a = {3,9,15,20,65,23,18,4,3,2,14,21};
+        System.out.println("Elements");
+   for (int c: a){
+       System.out.println("/t + c");
+       }
+        
+ 
+ArrayList<Integer> even = new ArrayList<>();
+Ar   
+   for (int b = 0; b<a.length; b++){
+       if (a[b] %2== 0){
+           even.add(a[b]);
+           
+       }
+   }
+   
+   
+   
+   
+   
+   
+    
+    
+    }
+    
+}
